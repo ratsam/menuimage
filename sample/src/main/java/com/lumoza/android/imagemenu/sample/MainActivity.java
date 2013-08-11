bcd32d872c3d0c432d2c993db961d2223dd9ebff
Maksim Zakharov <python.candidate@gmail.com>
2013-08-11 14:50:06 +0400

    Simple sample app

---
+++ sample/src/main/java/com/lumoza/android/imagemenu/sample/MainActivity.java
@@ -0,0 +1,11 @@
+package com.lumoza.android.imagemenu.sample;
+
+import android.app.Activity;
+
+/**
+ * @author Maksim Zakharov
+ * @author $Author$ (current maintainer)
+ * @since 1.0
+ */
+public class MainActivity extends Activity {
+}
