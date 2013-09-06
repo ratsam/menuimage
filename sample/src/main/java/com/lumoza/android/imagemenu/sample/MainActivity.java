ed02e3e4514cd76edf9147db6c539e54bee2e69c
Maksim Zakharov <python.candidate@gmail.com>
2013-09-06 17:46:26 +0400

    Added sample app.

--- sample/src/main/java/com/lumoza/android/imagemenu/sample/MainActivity.java
+++ sample/src/main/java/com/lumoza/android/imagemenu/sample/MainActivity.java
@@ -1,6 +1,11 @@
 package com.lumoza.android.imagemenu.sample;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import com.lumoza.android.ImageMenu;
+import com.lumoza.android.imagemenu.aq.ImageLoaderFactoryAQImpl;
+import com.lumoza.android.imagemenu.ImageLoaderFactoryHolder;
 
 /**
  * @author Maksim Zakharov
@@ -8,4 +13,22 @@ import android.app.Activity;
  * @since 1.0
  */
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final SharedPreferences preferences = getSharedPreferences("imagemenu", MODE_PRIVATE);
+        ImageLoaderFactoryHolder.setFactory(new ImageLoaderFactoryAQImpl(preferences));
+
+        setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        ((ImageMenu) findViewById(R.id.image_menu_2)).load("https://www.google.ru/images/srpr/logo4w.png_");
+        ((ImageMenu) findViewById(R.id.image_menu_3)).load("https://www.google.ru/images/srpr/logo4w.png");
+    }
 }
